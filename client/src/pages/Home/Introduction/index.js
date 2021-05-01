99e1e854f6e2d419f0a82eeeaeb19093656972b3
alyssa Zhan <zhanyan@utexas.edu>
2021-05-01 00:52:49 -0500

    convert introduction from class to function component & lift introduction up

--- client/src/pages/Home/Introduction/index.js
+++ client/src/pages/Home/Introduction/index.js
@@ -1,108 +1,49 @@
-import React, { Component } from "react";
+import React from "react";
 import Button from 'react-bootstrap/Button';
 import styles from "../styles.js"
 import {Jumbotron} from "react-bootstrap";
-import {withStyles} from "@material-ui/core/styles";
-const useStyles = theme => (styles)
-class Introduction extends React.Component {
+import { makeStyles } from '@material-ui/core/styles';
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            opened: false,
-            error: null,
-            isLoaded: false,
-            intro:null,
-        };
-        this.props = props;
-        this.toggleBox = this.toggleBox.bind(this);
-        this.componentDidMount=this.componentDidMount(this);
+const useStyles = makeStyles(styles);
 
-    }
-
-    toggleBox() {
-        const { opened } = this.state;
-        this.setState({
-            opened: !opened,
-        });
-    }
-
-    componentDidMount()  {
-        function getIntro(data) {
-            const intro =data["introduction"]
-            return intro
-
-        }
-
-        fetch("/api",{ mode: 'cors'})
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    const intro=getIntro(result["setup"])
-                    this.setState({
-                        isLoaded: true,
-                        intro:intro,
-                    });
-                    console.log("res is: ", result)
-                },
-                // Note: it's important to handle errors here
-                // instead of a catch() block so that we don't swallow
-                // exceptions from actual bugs in components.
-                (error) => {
-                    this.setState({
-                        isLoaded: true,
-                        error
-                    });
-                }
-            )
-    };
-    render() {
-        const {classes} =this.props;
-
-        var { title, children } = this.props;
-        const { opened, error, isLoaded ,intro} = this.state;
-        if (opened){
-            title ='Hide Tutorial';
-        }else{
-            title ='Show Tutorial';
-        }
-        if (error) {
-            return <div>Error: {error.message}</div>;
-        } else if (!isLoaded) {
-            return <div>Loading...</div>;
+export default function Introduction (props){
+    const classes = useStyles();
+    const [opened, setOpened] = React.useState(false);
+    const [title, setTitle]=React.useState('Show Tutorial');
+    function toggleBox(e) {
+        setOpened(!opened);
+        if (opened) {
+            setTitle('Hide Tutorial');
         } else {
-            return (
-                <div>
-                    <div className={classes.introContainer}>
-                        <Jumbotron  className={classes.jumbotron}>
-                            <h1 className={classes.introTitle}>Task Overview</h1>
-                            <p className={classes.introContent}>
-                                { intro}
-                            </p>
-                            <p>
-                                <div className={classes.buttonTitle}>
-                                    <Button
-                                        variant="outline-primary"
-                                        size="lg"
-                                        onClick={this.toggleBox}>
-                                        {title}
-                                    </Button>
-                                </div>
-                            </p>
-                        </Jumbotron>
-                    </div>
-
-
-                    {opened && (
-
-                        <div class="boxContent">
-                            {children}
-
-                        </div>
-                    )}
-                </div>
-            );
+            setTitle('Show Tutorial');
         }
     }
+    return (
+        <div>
+            <div className={classes.introContainer}>
+                <Jumbotron  className={classes.jumbotron}>
+                    <h1 className={classes.introTitle}>Task Overview</h1>
+                    <p className={classes.introContent}>
+                        { props.intro.intro}
+                    </p>
+                    <p>
+                        <div className={classes.buttonTitle}>
+                            <Button
+                                variant="outline-primary"
+                                size="lg"
+                                onClick={e => toggleBox(e)}>
+                                {title}
+                            </Button>
+                        </div>
+                    </p>
+                </Jumbotron>
+            </div>
+            {opened && (
+
+                <div class="boxContent">
+                    {props.children}
+                </div>
+            )}
+        </div>
+    );
 }
-export default withStyles(useStyles)(Introduction)
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -14,18 +14,30 @@ export default function Home() {
         steps:null,
         imgUrl:null,
     })
+    const [intro, setIntro]=useState({
+        intro:null,})
     const [hasError, setHasError] = useState(false)
     const [loading, setLoading]= useState(false)
     useEffect(() => {
-        setLoading(true)
+        setLoading(true);
+        function getIntro(data) {
+            const intro =data["introduction"]
+            return intro
+
+        }
         fetch("/api",{ mode: 'cors'}).then(res=>res.json()).then(
             (result) => {
+                // const intro=getIntro(result["setup"]);
                 console.log("start setState");
                 setTutorial({
                     steps:result["setup"]["tutorial"]["steps"],
                     imgUrl:result["setup"]["tutorial"]["imgUrl"]
                 });
+                setIntro({
+                    intro:result["setup"]["introduction"],
+                });
                 setLoading(false);
+                console.log("intro is",intro)
             }
         ).catch(err => {
             setHasError(err)
@@ -40,9 +52,10 @@ export default function Home() {
         return (
             <div className={classes.homeContainer}>
                 <div className={classes.introTutorial}>
-                    <Intro title="Show tutorial">
+                    <Intro title="Show tutorial"
+                           intro={intro}>
                         <Tutorial tutorial={tutorial} />
-                    </Intro>
+                    </Intro >
                 </div>
                 <div className={classes.annotateContainer}>
                     < SubmitHIT />
